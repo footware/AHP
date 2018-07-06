37dbd6353dc13374f87b6d2cb1f8803d1ec2b1ff
8009 <csst18009@g.nihon-u.ac.jp>
2018-07-06 17:33:05 +0900

    first commimt

---
+++ test.js
@@ -0,0 +1,57 @@
+function test() {
+    var data = makeData();
+    var table = makeTable(data);
+    var button = makeButton();
+    document.body.appendChild(table);
+    document.body.appendChild(button);
+}
+
+function makeButton() {
+    button = document.createElement("button");
+    text = document.createTextNode("next");
+    button.appendChild(text);
+
+    return button;
+
+}
+
+function makeRange() {
+    var range = document.createElement("input");
+    range.setAttribute("type", "range");
+    range.setAttribute("max", "10");
+    range.setAttribute("value", "5");
+
+    return range;
+}
+
+function makeTable(data) {
+
+    var table = document.createElement("table");
+
+    for (i = 0; i < data.length; i++) {
+        row = table.insertRow(-1);
+        for (j = 0; j < data[0].length; j++) {
+            td = row.insertCell(-1);
+            td.appendChild(data[i][j]);
+        }
+    }
+    return table;
+}
+
+function makeData() {
+
+    var x = document.getElementById("criteria1").value;
+    var y = document.getElementById("criteria2").value;
+    var z = document.getElementById("criteria3").value;
+
+    var range1 = makeRange();
+    var range2 = makeRange();
+    var range3 = makeRange();
+
+    var data =
+        [[document.createTextNode(x), range1, document.createTextNode(y)],
+        [document.createTextNode(y), range2, document.createTextNode(z)],
+        [document.createTextNode(z), range3, document.createTextNode(x)]];
+
+    return data;
+}
